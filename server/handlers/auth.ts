2addd927c04cedfd1237fa1281007f79c0769bf0
footlessbird <arslongavitabrevis0818@gmail.com>
2020-05-30 14:45:15 -0700

    refactoring auth

--- server/handlers/auth.ts
+++ server/handlers/auth.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
-import jwt from "jsonwebtoken";
+import axios from "axios";
+
 import User from "../models/user";
 
 const currentUser = (req, res, next) => {
@@ -14,25 +15,79 @@ const logout = (req, res) => {
   res.redirect("/");
 };
 
+const github = (req, res) => {
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${clientId}`
+  );
+};
+
+const githubCallback = async (req, res, next) => {
+  const body = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_CLIENT_SECRET,
+    code: req.query.code,
+  };
+  const opts = { headers: { accept: "application/json" } };
+
+  const getAccessToken = await axios.post(
+    `https://github.com/login/oauth/access_token`,
+    body,
+    opts
+  );
+  const { access_token } = getAccessToken.data;
+  console.log("token??", access_token);
+
+  const uri = `https://api.github.com/user`;
+  const auth = `bearer ${access_token}`;
+
+  async function getUserData() {
+    try {
+      const userData = await axios.get(uri, {
+        headers: {
+          Authorization: auth,
+        },
+      });
+      // console.log(userData);
+      return userData.data;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  const userData = await getUserData();
+  // console.log("user??", userData);
+  const { login, id, email } = userData;
+
+  try {
+    // check whether the user in the db or not
+    const existingUser = await User.findOne({ githubId: id });
+    if (existingUser) {
+      req.user = existingUser;
+      next();
+    } else {
+      // store into mongodb
+      const newUser = await new User({
+        githubId: id,
+        email,
+        username: login,
+      }).save();
+      req.user = newUser;
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
 // local login
 const login = (req, res, next) => {
   console.log("login handler called");
   console.log("login user ", req.user);
   const user = req.user;
-  if (!user) throw new Error("User does not exist");
-  // const secretOrKey = process.env.SECRET;
-  const token = jwt.sign({ id: user.id }, process.env.SECRET, {
-    expiresIn: 3600,
-  });
-  if (!token) throw new Error("JWT sign error");
-  res.status(200).json({
-    token,
-    user: {
-      id: user.id,
-      email: user.email,
-      username: user.username,
-    },
-  });
+  if (!user) return res.status(400).send({ message: "User does not exist." });
+  next();
 };
 
 const register = async (req, res, next) => {
@@ -81,4 +136,12 @@ const test = (req, res) => {
   });
 };
 
-export default { currentUser, logout, register, login, test };
+export default {
+  currentUser,
+  logout,
+  register,
+  login,
+  github,
+  githubCallback,
+  test,
+};
--- server/middlewares/generateAuthToken.ts
+++ server/middlewares/generateAuthToken.ts
@@ -6,13 +6,14 @@ function getJwt(userId) {
   const token = jwt.sign({ id: userId }, process.env.SECRET, {
     expiresIn: 3600,
   });
+  if (!token) throw new Error("JWT sign error");
   return token;
 }
 
 export default function setUserWithToken(req, res) {
   const token = getJwt(req.user.id);
   const { id, email, username } = req.user;
-  res.status(200).json({
+  return res.status(200).json({
     token,
     user: {
       id,
