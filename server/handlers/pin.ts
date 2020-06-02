cb00508be9a142742e82250528803cabcb8c0e83
footlessbird <arslongavitabrevis0818@gmail.com>
2020-06-01 18:49:58 -0700

    pin CRU

--- server/handlers/pin.ts
+++ server/handlers/pin.ts
@@ -33,4 +33,60 @@ const getPins = async (req, res, next) => {
   }
 };
 
-export default { createPin, getPins };
+const save = async (req, res, next) => {
+  const pinId = req.params.id.toString();
+  try {
+    const pin = await Pin.findById(pinId);
+    console.log(`${pin.user._id} owns this pin `);
+    console.log("req.user.id", req.user.id);
+    console.log("same user? ", pin.user._id === req.user.id);
+    console.log("pin created by type", typeof pin.user._id);
+    console.log("req.user.id type", typeof req.user.id);
+    console.log(
+      "if toString then same user? ",
+      pin.user._id.toString() === req.user.id
+    );
+    if (pin.user._id.toString() === req.user.id) {
+      throw new Error("Cannot pin my own");
+    } else if (pin.savedBy.includes(req.user.id)) {
+      throw new Error("Already saved");
+    } else {
+      pin.savedBy.push(req.user.id);
+      await pin.save();
+      return res.status(200).json(pin);
+    }
+  } catch (err) {
+    return next({
+      status: 400,
+      message: err.message,
+    });
+  }
+};
+
+const usersPins = async (req, res, next) => {
+  const userId = req.user.id;
+  try {
+    const myPins = await Pin.find({ user: req.user.id });
+    const pins = await Pin.find();
+    const savedPins = pins.filter((pin) => pin.savedBy.includes(userId));
+    const result = [...myPins, ...savedPins];
+
+    const returnUsersPins = () => {
+      return result.map((pin) => {
+        const { user, imgLink, imgDescription, savedBy } = pin;
+        return {
+          user,
+          imgLink,
+          imgDescription,
+          savedBy,
+        };
+      });
+    };
+    console.log("returnUsersPins", returnUsersPins);
+    return res.status(200).json(returnUsersPins());
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export default { createPin, getPins, save, usersPins };
--- server/routes/pin.ts
+++ server/routes/pin.ts
@@ -6,8 +6,12 @@ import handle from "../handlers";
 const router = Router();
 const noSessionForJwt = passport.authenticate("jwt", { session: false });
 
-const { createPin, getPins } = handle;
+const { createPin, getPins, save, usersPins } = handle;
 
 router.route("/").get(getPins).post(noSessionForJwt, createPin);
+
+router.get("/user", noSessionForJwt, usersPins);
+
+router.route("/:id").post(noSessionForJwt, save);
 
 export default router;
